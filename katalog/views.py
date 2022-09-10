459f27cebfa9a535dee8e9c41ec04f6251a95088
Veivel <25278964+Veivel@users.noreply.github.com>
2022-09-10 17:54:15 +0700

    katalog done

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    
+    catalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog': catalog,
+        'nama': 'Givarrel Veivel Pattiwael',
+        'npm': '2106640341'
+    }
+    
+    return render(request, 'katalog.html', context)
