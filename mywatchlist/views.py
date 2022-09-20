8f1246de52a65e1d1b275df4c25f184acde02054
Veivel <25278964+Veivel@users.noreply.github.com>
2022-09-20 13:52:05 +0700

    fix tugas 3 for deploy

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -7,30 +7,29 @@ from mywatchlist.models import MyWatchlist
 import pandas as pd
 
 # Create your views here.
-def CustomParser(data):
-    ''' Source: https://stackoverflow.com/questions/43668533/access-a-json-column-with-pandas '''
-    import json
-    j1 = json.loads(data)
-    return j1
+# def CustomParser(data):
+#     ''' Source: https://stackoverflow.com/questions/43668533/access-a-json-column-with-pandas '''
+#     import json
+#     j1 = json.loads(data)
+#     return j1
 
-def get_watch_balance():    
-    ''' Returns an integer, number of movies watched - number of movies not watched.
-    Positive means more movies watched, negative means more movies not watched.'''
+# def get_watch_balance():    
+#     ''' Returns an integer, number of movies watched - number of movies not watched.
+#     Positive means more movies watched, negative means more movies not watched.'''
     
-    df = pd.read_json('mywatchlist/fixtures/initial_watchlist_data.json')
-    df = pd.json_normalize(df['fields'])
-    true_num = df[df['watched'] == True].shape[0]
-    false_num = df[df['watched'] == False].shape[0]
+#     df = pd.read_json('mywatchlist/fixtures/initial_watchlist_data.json')
+#     df = pd.json_normalize(df['fields'])
+#     true_num = df[df['watched'] == True].shape[0]
+#     false_num = df[df['watched'] == False].shape[0]
     
-    return true_num - false_num
+#     return true_num - false_num
 
 def show_html(request):
-    
     list_watchlist = MyWatchlist.objects.all()
     context = {
         'list_watchlist': list_watchlist,
         'title': 'Tugas 3 PBP-D',
-        'num_balance': get_watch_balance()
+        'num_balance': len(list_watchlist.filter(watched=True)) - len(list_watchlist.filter(watched=True))
     }
     
     return render(request, 'watchlist.html', context)
